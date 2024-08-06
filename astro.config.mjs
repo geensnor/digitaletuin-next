387ac47f2c5c10756d3da2f5cf617015e30efe7d
Joris <jorisreijbroek@gmail.com>
2024-08-06 16:29:22 +0200

    YAML package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightLinksValidator from "starlight-links-validator";
+import yaml from "@rollup/plugin-yaml";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [yaml()],
+  },
   integrations: [
     starlight({
       plugins: [
