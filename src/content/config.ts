17338cc00dd9e1a1b1edb1dd7e440fac7586da6b
Joris <jorisreijbroek@gmail.com>
2024-08-10 08:14:02 +0000

    recepten

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,12 @@
-import { defineCollection } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
 	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+	recepten: defineCollection({ type: 'content', schema: z.object({
+    	title: z.string(),
+		recipeYield: z.number().optional(),
+		recipeCategory: z.enum(['hoofdgerecht', 'voorgerecht', 'ontbijt']).optional(),
+  }), }),
 };
