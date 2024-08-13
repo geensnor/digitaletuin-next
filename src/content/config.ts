5f0426186a98ccb7d44ae866d5f8a7fff8b78cb8
Joris <jorisreijbroek@gmail.com>
2024-08-13 23:49:27 +0200

    recepten pagina's

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,12 +5,14 @@ export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
 	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 	recepten: defineCollection({ type: 'content', schema: z.object({
-    	title: z.string(),
+    	name: z.string(),
 		recipeYield: z.number().optional(),
-		recipeCategory: z.enum(['hoofdgerecht', 'voorgerecht', 'ontbijt', 'lunchgerecht']).optional(),
+		recipeCategory: z.enum(['hoofdgerecht', 'voorgerecht', 'ontbijt', 'lunch', 'nagerecht', 'soep']).optional(),
+		recipeCuisine: z.string().optional(),
 		cookTime: z.string().duration().optional(),
 		prepTime: z.string().duration().optional(),
 		totalTime: z.string().duration().optional(),
 		calories: z.number().optional(),
+		author: z.string().optional(),
   }), }),
 };
