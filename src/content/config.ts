f0607d2c9876597dc6907794f48de0cd28195c8e
Joris <jorisreijbroek@gmail.com>
2024-08-16 09:49:28 +0000

    dieetwensen!

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,17 +2,44 @@ import { z, defineCollection } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
-	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
-	recepten: defineCollection({ type: 'content', schema: z.object({
-    	name: z.string(),
-		recipeYield: z.number().optional(),
-		recipeCategory: z.enum(['hoofdgerecht', 'voorgerecht', 'ontbijt', 'lunch', 'nagerecht', 'soep']).optional(),
-		recipeCuisine: z.string().optional(),
-		cookTime: z.string().duration().optional(),
-		prepTime: z.string().duration().optional(),
-		totalTime: z.string().duration().optional(),
-		calories: z.number().optional(),
-		author: z.string().optional(),
-  }), }),
+  docs: defineCollection({ schema: docsSchema() }),
+  i18n: defineCollection({ type: "data", schema: i18nSchema() }),
+  recepten: defineCollection({
+    type: "content",
+    schema: z.object({
+      name: z.string().describe("Naam van het gerecht"),
+      recipeYield: z.number().optional(),
+      recipeCategory: z
+        .enum([
+          "hoofdgerecht",
+          "voorgerecht",
+          "ontbijt",
+          "lunch",
+          "nagerecht",
+          "soep",
+        ])
+        .optional(),
+      recipeCuisine: z.string().optional(),
+      cookTime: z.string().duration().optional(),
+      prepTime: z.string().duration().optional(),
+      totalTime: z.string().duration().optional(),
+      calories: z.number().optional(),
+      author: z.string().optional(),
+      suitableForDiet: z.array(z
+        .enum([
+          "DiabeticDiet",
+          "GlutenFreeDiet",
+          "HalalDiet",
+          "HinduDiet",
+          "KosherDiet",
+          "LowCalorieDiet",
+          "LowFatDiet",
+          "LowLactoseDiet",
+          "LowSaltDiet",
+          "VeganDiet",
+          "VegetarianDiet",
+        ]))
+        .optional(),
+    }),
+  }),
 };
